afc6035f63e11e70f4aa5591a701c6f04ecc579b
apineda-eql <a.pineda.ti@equitylink.com.mx>
2024-02-01 11:48:35 -0600

    model, seeders, factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Curso::factory(10)->create();
+        //$this->call(CursoSeeder::class);
     }
 }
