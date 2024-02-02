64e784703a86eeea2968f3ea412ae514ab8c8065
apineda-eql <a.pineda.ti@equitylink.com.mx>
2024-02-02 10:57:56 -0600

    accesores y mutadores

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
     use HasFactory;
+
+    protected function name(): Attribute{
+        return new Attribute(
+            set: fn($value) => strtolower($value),
+            get: fn($value) => ucwords($value)
+        );
+    }
+
+    // forma de definir un accesor en versiones anteriores
+    // public function getNameAttribute($value): Attribute {
+    //     return ucwords($value);
+    // }
+
+    // forma de definir un mutador en versiones anteriores
+    // public function setNameAttribute($value): Attribute{
+    //     $this->attributes['name'] = strtolower($value);
+    // }
 }
