a9a7869faca527936dd6c4485d8b291b84ddfb1c
apineda-eql <a.pineda.ti@equitylink.com.mx>
2024-02-05 10:11:30 -0600

    form request

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCurso;
 use App\Models\Curso;
 use Illuminate\Http\Request;
 
@@ -16,13 +17,8 @@ class CursoController extends Controller
         return view('cursos.create');
     }
 
-    public function store(Request $request){
-        $request->validate([
-            'name' => 'required',
-            'category' => 'required',
-            'description' => 'required',
-        ]);
-
+    public function store(StoreCurso $request){
+        
         $curso = new Curso();
         $curso->name = $request->name;
         $curso->category = $request->category;
@@ -44,12 +40,7 @@ class CursoController extends Controller
         return view('cursos.edit', ['curso'=>$curso]);
     }
 
-    public function update(Request $request, Curso $curso){
-        $request->validate([
-            'name' => 'required',
-            'category' => 'required',
-            'description' => 'required',
-        ]);
+    public function update(StoreCurso $request, Curso $curso){
         
         $curso->name = $request->name;
         $curso->category = $request->category;
---
+++ app/Http/Requests/StoreCurso.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCurso extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:3',
+            'category' => 'required',
+            'description' => 'required',
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'description.required' => 'La descripción es requerida para el curso.',
+        ];
+    }
+
+    public function attributes(): array{
+        return [
+            'name' => 'nombre del curso',
+        ];
+    }
+
+}
