40f5c937663874457d8f67cc0b51f2abbfddcf52
Angel Pineda <angel@Angels-MacBook-Air.local>
2024-03-05 12:49:44 -0600

    send mails

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\ContactanosMailable;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
@@ -10,3 +12,11 @@ Route::get('/', HomeController::class)->name('home');
 Route::resource('cursos', CursoController::class);
 
 Route::view('nosotros', 'static.nosotros')->name('nosotros');
+
+Route::get('contactanos', function(){
+
+    Mail::to('angel@test.com')->send(new ContactanosMailable());
+
+    return 'Mensaje enviado';
+
+})->name('contactanos');
